07e8458a1f03a7b5748f731a1b183748cc4c3515
Piotr Dudkiewicz <piotr.dudkiewicz@defra.onmicrosoft.com>
2019-08-18 14:41:12 +0200

    Removed files for review

--- src/main/java/com/za5groszy/foundation/market/domain/exception/InsufficientAmountOfBidsException.java
+++
@@ -1,7 +0,0 @@
-package com.za5groszy.foundation.market.domain.exception;
-
-public class InsufficientAmountOfBidsException extends Exception {
-    public InsufficientAmountOfBidsException() {
-        super("Insufficient amount of bids.");
-    }
-}
--- src/main/java/com/za5groszy/foundation/market/readmodel/MarketReadModelRepository.java
+++
@@ -1,9 +0,0 @@
-package com.za5groszy.foundation.market.readmodel;
-
-import com.za5groszy.foundation.market.sharedkernel.item.Item;
-
-import java.util.List;
-
-public interface MarketReadModelRepository {
-    public List<Item> getCurrentState();
-}
